b063a61768f0dca827b287927d121c8037b27adc
Piotr Zieliński <piotr.zielinski.1994@gmail.com>
2025-09-16 17:12:51 +0200

    Initial commit

---
+++ src/main/kotlin/com/example/test_aws/TestAwsApplication.kt
@@ -0,0 +1,11 @@
+package com.example.test_aws
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TestAwsApplication
+
+fun main(args: Array<String>) {
+	runApplication<TestAwsApplication>(*args)
+}
